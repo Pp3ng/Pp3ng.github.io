d02819a1387b4d4e70109fa68f692f9dcfc1ac52
ZH0UU <pp3ng@outlook.com>
2024-10-14 18:41:59 +0900

    Button

--- scripts.js
+++ scripts.js
@@ -50,7 +50,10 @@ $(document).ready(function () {
 
     $(document).ready(function () {
         // Scroll to Top Button
-        const scrollToTopBtn = $('<button id="back-to-top" aria-label="Scroll to top"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg></button>').css({
+        const scrollToTopBtn = $('<button id="back-to-top" aria-label="Scroll to top">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ' +
+            'fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">' +
+            '<polyline points="18 15 12 9 6 15" stroke="white"></polyline></svg></button>').css({
             position: 'fixed',
             bottom: '20px',
             right: '20px',
